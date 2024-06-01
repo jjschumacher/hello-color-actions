a23df9a5735812f48d4ad99218c1f0c16550a0cf
jjschumacher <jjschumacher@gmail.com>
2024-06-01 08:03:52 -0400

    updated hello world message

--- hello.js
+++ hello.js
@@ -1,4 +1,4 @@
-const hello = () => "Hello World from the Office Hours in the terminal!";
+const hello = () => "Hello World from my home office in the middle of no where";
 console.log(hello());
 exports.hello = hello;
 
